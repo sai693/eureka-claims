88a0802302738960e6bb673ba05f681d33625b1f
saikumar gurugbelli <gurugubellisaikumar68@gmail.com>
2022-12-13 15:25:17 +0530

    dg

---
+++ cnn.coudgeeks.ca/src/test/java/cnn/coudgeeks/ca/ApplicationTests.java
@@ -0,0 +1,13 @@
+package cnn.coudgeeks.ca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
